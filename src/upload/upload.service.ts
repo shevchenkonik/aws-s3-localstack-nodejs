c830085d85da435c2ff68812c8312b7f509148d5
Nik <shevchenko.nik@icloud.com>
2025-02-24 10:52:22 +0100

    S3 Client prepared into UploadService

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
 @Injectable()
 export class UploadService {
+  private readonly s3Client: S3Client;
+
+  constructor() {
+    this.s3Client = new S3Client({
+      endpoint: '', // TODO: replace with your endpoint
+      region: '', // TODO: replace with your region
+      credentials: {
+        accessKeyId: '', // TODO: replace with your access key ID
+        secretAccessKey: '', // TODO: replace with your secret access key
+      },
+    });
+  }
+
   async upload(fileName: string, file: Buffer) {
-    // TODO: implement upload logic
+    await this.s3Client.send(
+      new PutObjectCommand({
+        Bucket: '', // TODO: replace with your bucket name
+        Key: fileName,
+        Body: file,
+      }),
+    );
   }
 }
