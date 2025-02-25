a5e09542a31754fd3c631b1b3df8f81ae3b20a1a
Nik <shevchenko.nik@icloud.com>
2025-02-25 18:53:18 +0100

    Added default validation for config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { UploadModule } from './upload/upload.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UploadModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        AWS_USERNAME: Joi.string().required(),
+        AWS_S3_GATEWAY: Joi.string().required(),
+        AWS_S3_REGION: Joi.string().required(),
+        AWS_S3_BUCKET: Joi.string().required(),
+        AWS_ACCESS_KEY: Joi.string().required(),
+        AWS_SECRET_ACCESS_KEY: Joi.string().required(),
+      }),
+      isGlobal: true,
+    }),
+    UploadModule,
+  ],
 })
 export class AppModule {}
